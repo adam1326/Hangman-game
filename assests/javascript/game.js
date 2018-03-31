9f3cdd14d5ee966ffd96ec5f7a6ae75faf7e24bc
adam1326 <adam.mccain@gmail.com>
2018-03-31 18:29:08 -0400

    updated code with spaces and game logic

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,17 +1,17 @@
 // Start of the JavaScript with a prompt
-var userName=prompt("Hello, Whats your name?");
+var userName = prompt("Hello, Whats your name?");
 alert("Hello " + userName)
 alert("Are you ready for Star Wars Hangman?")
-window.onload = function(){
-document.getElementById("Name").innerHTML = userName + " Welcome to:";
+window.onload = function () {
+    document.getElementById("Name").innerHTML = userName + " Welcome to:";
 }
 
 
 // Start of the game
 // Characters of the movie Star Wars
-var characters = ["Jar Jar Binks", "Obi-Wan Kenobi", "Leia", "Chewbecca", "Han Solo", "Darth Vader", "Yoda", "Luke Skywalker", "Anakin Skywalker" ];
+var characters = ["Jar Jar Binks", "Obi-Wan Kenobi", "Leia", "Chewbecca", "Han Solo", "Darth Vader", "Yoda", "Luke Skywalker", "Anakin Skywalker"];
 // English Alphabet with special characters
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", " "];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-"];
 var lives = 12;
 
 // Game state
@@ -19,45 +19,71 @@ var wins = 0;
 var losses = 0;
 var guesses = [];
 var targetedCharacter = "";
+var letterBlanks = [];
+
+function pickRandomLetter() {
+    for (var i = 0; i < character.length; i++) {
+
+        if (character.charAt(i) != ' ') {
+            letterBlanks.push("_");
+
+        } else {
+            letterBlanks.push(" ")
+        }
+    } console.log(letterBlanks)
+}
+
 
 //JS into HTML
-    function updateOutput() {
-        var html =
+function updateOutput() {
+    var html =
         "<p>Lives: " + lives + "</p>" +
-        "<p>Current Word: _ " + targetedCharacter + "</p>" +
-        // "<p>_ _ _ _ " + targetedCharacter + "</p>" +
+        "<p>Current Word: " + letterBlanks + "</p>" +
         "<p>Letters Guessed: " + guesses + "</p>" +
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>";
-        document.querySelector("#myGameArea").innerHTML = html;
+    document.querySelector("#myGameArea").innerHTML = html;
 
-    }
-updateOutput();
-// Picking random Star Wars Character
-function pickRandomCharacters() {
-    for (var i = 0; i = characters; i++)
-    var characters = characters[Math.floor(Math.random() * characters.length)];
-    var characters = targetedCharacter;
-    console.log(characters);
-    return characters;
 }
 
-document.onkeyup = function(event) {
+// Picking random Star Wars Character
+
+var character = characters[Math.floor(Math.random() * characters.length)];
+console.log(character);
+
+pickRandomLetter();
+updateOutput();
+// Putting letters from that character
+
+
+
+
+
+document.onkeyup = function (event) { 
     var guess = event.key.toLowerCase();
-    if (! alphabet.includes(guess)) { return;}
-    if (guess.includes(guess)) { return;}
+    console.log(guess);
+    if (!alphabet.includes(guess)) { return; }
+    if (guesses.includes(guesses)) { return; }
 
     guesses.push(guess);
     guesses.sort();
-    if (guess === targetedCharacter) {
+    for (var i = 0; i < character.length; i++) {
+        if (guess === character.charAt(i).toLowerCase()) {
+            letterBlanks[i] = guess;
+        } console.log(guess)
+        console.log(character.charAt(i))
+    }
+    if (guess === character) {
         wins++;
+        guesses = [];
+        targetedCharacter = pickRandomLetter();
 
         //Reset game
         guesses = [];
         targetedCharacter = pickRandomCharacter();
     } else {
-        //innocent guess
-        if (guesses.lengeth >= lives) {
+        //incorret guess
+        if (guesses.length >= lives) {
             losses++;
             guesses = [];
             targetedCharacter = pickRandomCharacter();
@@ -67,7 +93,7 @@ document.onkeyup = function(event) {
     // update display
     updateOutput();
 }
-    
+
 // // Start of the hangman
 // function startGame() {
 //     characters = characters[Math.floor(Math.random() * characters.length)]
