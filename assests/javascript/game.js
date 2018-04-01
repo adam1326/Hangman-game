fe4f674654a2ceaf6d5cb3379efcd88e3aaecef3
adam1326 <adam.mccain@gmail.com>
2018-03-31 22:20:38 -0400

    updated code with spaces and game logic

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -10,29 +10,34 @@ window.onload = function () {
 // Start of the game
 // Characters of the movie Star Wars
 var characters = ["Jar Jar Binks", "Obi-Wan Kenobi", "Leia", "Chewbecca", "Han Solo", "Darth Vader", "Yoda", "Luke Skywalker", "Anakin Skywalker"];
-// English Alphabet with special characters
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-"];
-var lives = 12;
+var maxLives = 12;
 
 // Game state
+
 var wins = 0;
 var losses = 0;
+var lives = maxLives;
 var guesses = [];
-var targetedCharacter = "";
 var letterBlanks = [];
 
-function pickRandomLetter() {
-    for (var i = 0; i < character.length; i++) {
-
+// Picking random Star Wars Character
+function reset() {
+	guesses = [];
+	character = characters[Math.floor(Math.random() * characters.length)];
+	lives = maxLives;
+	letterBlanks = [];
+	
+	for (var i = 0; i < character.length; i++) {
         if (character.charAt(i) != ' ') {
             letterBlanks.push("_");
-
         } else {
-            letterBlanks.push(" ")
+            letterBlanks.push(" ");
         }
-    } console.log(letterBlanks)
+    }
+	console.log(character);
 }
-
+reset();
+updateOutput();
 
 //JS into HTML
 function updateOutput() {
@@ -43,63 +48,48 @@ function updateOutput() {
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>";
     document.querySelector("#myGameArea").innerHTML = html;
-
 }
 
-// Picking random Star Wars Character
-
-var character = characters[Math.floor(Math.random() * characters.length)];
-console.log(character);
-
-pickRandomLetter();
-updateOutput();
 // Putting letters from that character
-
-
-
-
-
-document.onkeyup = function (event) { 
+document.onkeyup = function (event) {
     var guess = event.key.toLowerCase();
     console.log(guess);
+
+	var alphabet = "abcdefghijklmnopqrstuvwxyz-";
     if (!alphabet.includes(guess)) { return; }
-    if (guesses.includes(guesses)) { return; }
+    if (guesses.includes(guess)) { return; }
 
     guesses.push(guess);
     guesses.sort();
-    for (var i = 0; i < character.length; i++) {
-        if (guess === character.charAt(i).toLowerCase()) {
-            letterBlanks[i] = guess;
-        } console.log(guess)
-        console.log(character.charAt(i))
-    }
-    if (guess === character) {
-        wins++;
-        guesses = [];
-        targetedCharacter = pickRandomLetter();
 
-        //Reset game
-        guesses = [];
-        targetedCharacter = pickRandomCharacter();
-    } else {
-        //incorret guess
-        if (guesses.length >= lives) {
-            losses++;
-            guesses = [];
-            targetedCharacter = pickRandomCharacter();
+	if (character.toLowerCase().includes(guess)) {
+		// we've made a correct guess
 
-        }
-    }
+		// good guess, now need to update letterBlanks
+		for (var i = 0; i < character.length; i++) {
+			if (character[i].toLowerCase() == guess.toLowerCase()) {
+				letterBlanks[i] = character[i];
+			}
+		}
+
+
+	} else {
+		// incorrect guess
+		lives--;
+	}
+
+
+	if (letterBlanks.join("") == character) {
+		// won
+		wins++;
+		reset();
+	}
+	if (lives <= 0) {
+		// died.
+		losses++;
+		reset();
+	}
+
     // update display
     updateOutput();
 }
-
-// // Start of the hangman
-// function startGame() {
-//     characters = characters[Math.floor(Math.random() * characters.length)]
-//     console.log(characters);
-//     console.log(characters.length);
-//     for(var i = 0; i < characters.length; i++) {
-//         console.log(i);
-//         underScores.push('_');
-
